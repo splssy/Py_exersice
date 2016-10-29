d8cd018bf6bbfd830224895353c58109712cfd34
songsiyu <songsiyu1993@gmail.com>
2016-10-29 23:12:25 +0800

    automorphic v2

--- automorphic.py
+++ automorphic.py
@@ -1,10 +1,3 @@
 
-def isautomorphic(n):
-    l = len(str(n))
-    k = n ** 2
-    t = k % (10 ** l)
-    return t == n
-for x in range(10000):
-    if isautomorphic(x):
-       print(x)
+print([n for n in range(1,10000) if n * n % (10 ** len(str(n))) == n])
 
